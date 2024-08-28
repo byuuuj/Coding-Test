aecfffa61190a44996a11210749d849424c00aea
Yu Jeong <91941700+byuuuj@users.noreply.github.com>
2024-08-29 00:37:56 +0900

    Update 짝수 홀수 개수.java

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -21,5 +21,14 @@ class Solution {
         
         
         return answer;
+
+        // 다른 사람이 한 풀이
+        // 짝수 홀수 2의 나머지는 0 아니면 1만 나오기때문에 짝수이면 배열의 0번째에 ++ , 홀수이면 배열의 1번째에 ++ 하도록 작성
+        // int[] answer = new int[2];
+
+        // for(int i = 0; i < num_list.length; i++)
+        //     answer[num_list[i] % 2]++;
+
+        // return answer;
     }
 }
