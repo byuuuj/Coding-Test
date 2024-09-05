abe0d61a1cb86c654b2c05eb1355a4481faeeb27
Yu Jeong <91941700+byuuuj@users.noreply.github.com>
2024-09-06 00:07:23 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        
+        for(int a = 0; a < array.length; a++){
+            if(array[a] > height){
+                answer ++;
+            }
+        }
+        
+        return answer;
+    }
+}
